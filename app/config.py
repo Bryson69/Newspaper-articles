98240942194d25032502fe08e73388890194cff5
Bryson69 <brysonmundia@gmail.com>
2020-07-05 22:23:22 +0300

    Extend base.htnl,import marcos, Add styles, Add block content and endblock

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ class Config:
     General configuration parent class
     '''
 
-    ARTICLE_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    # ARTICLE_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+   	# ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     ARTICLE_API_KEY = '95b3000b62764125a9981921ca505d4a'
 
