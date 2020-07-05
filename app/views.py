11aea884a5a2a825194ba8ace02fda7540ce0699
Bryson69 <brysonmundia@gmail.com>
2020-07-05 22:34:29 +0300

    Add imports and functions

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_articles
+from .request import get_articles,get_article
 
 
 # Views
@@ -10,7 +10,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    scoop_article = get_articles('scoop')
+    
     politics_article = get_articles('politics')
     title = 'The best article page'
     return render_template('index.html', title = title)
@@ -18,10 +18,13 @@ def index():
 
 
     
-# @app.route('/article/<int:article_id>')
-# def article(article_id):
-#     '''
-#     Views article page that returns the atricle and its data
-#     '''
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    '''
+    Views article page that returns the atricle and its data
+    '''
 
-#     return render_template('article.html', id = article_id)
+    article = get_article(id)
+    title = f'{article.title}'
+
+    return render_template('article.html', title = title, article = article)
