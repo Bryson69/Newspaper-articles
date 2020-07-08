3f995f4514ba09411033435deec3e46d06dc8a05
Bryson69 <brysonmundia@gmail.com>
2020-07-08 21:10:19 +0300

    Add class for DevConfig and add config options

--- config.py
+++ config.py
@@ -10,4 +10,16 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
 
