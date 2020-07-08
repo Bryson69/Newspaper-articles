dbc817d9ccdf12e67835ed76b524a20e7f89f97a
Bryson69 <brysonmundia@gmail.com>
2020-07-08 22:27:56 +0300

    Add import for flask, bootstrsap, confi.py and add function for create app

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
+
+bootstrap = Bootstrap()
 
 
-# Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
+    # create the app configurations
+    app.config.from_object(config_options[config_name])
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
-
-from app import views
+    return app
